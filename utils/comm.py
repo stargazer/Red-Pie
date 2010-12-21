7ce51025d659e166b093ba8567ce090eeb8ea22b
stargazer <already.late@gmail.com>
2010-12-22 00:14:00 +0100

    Function constructMessage() moved to this module

--- utils/comm.py
+++ utils/comm.py
@@ -19,3 +19,38 @@ def getLine(sock):
     sock.recv(1)                  # Consume the remaining \n character
     return line
     
+
+def constructMessage(command, args = []):
+    """ Constructs the appropriate message, 
+        that will be send to the redis server.
+        The message represents a database query
+        
+        @param command: Database command to execute
+        @param: List of arguments for command
+
+        Messages are in the form:
+        *<num arguments>\r\n
+        $<length of command>\r\n
+        command\r\n
+        $<length of arg>\r\n
+        argument\r\n
+        $length of arg>\r\n
+        argument\r\n
+        ...
+        ...\r\n
+    """
+    part_1 = [ \
+        "*" + str(len(args) + 1), \
+        "$" + str(len(command.encode("utf8"))), \
+        command \
+    ]
+    
+    part_2 = []
+    for arg in args:
+        arg = str(arg)
+        part_2.append("$" + str(len(arg.encode("utf8"))))
+        part_2.append(arg)
+
+    message = "\r\n".join(part_1 + part_2) + "\r\n"
+    return message.encode("utf8")
+    #"\r\n".join(message) + "\r\n"
